543dd37819e9a72bd17ac351541423ab6225c220
Isra de Leon <isra.frontend@gmail.com>
2026-03-27 11:23:10 -0600

    create the ktor http client factory

---
+++ KMPAppv2/composeApp/src/commonMain/kotlin/com/isradeleon/kmpappv2/common/network/HttpClientFactory.kt
@@ -0,0 +1,54 @@
+package com.isradeleon.kmpappv2.common.network
+
+import io.ktor.client.HttpClient
+import io.ktor.client.engine.HttpClientEngine
+import io.ktor.client.plugins.HttpTimeout
+import io.ktor.client.plugins.cache.HttpCache
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.client.plugins.defaultRequest
+import io.ktor.http.ContentType
+import io.ktor.http.contentType
+import io.ktor.serialization.kotlinx.json.json
+import kotlinx.serialization.json.Json
+
+/**
+ * Factory design pattern for the HTTP client.
+ *
+ * This is implemented this way, because the underlying
+ * networking engine may vary from Android to iOS.
+ *
+ * We can still share common configurations.
+ */
+object HttpClientFactory {
+    fun create(
+        engine: HttpClientEngine
+    ): HttpClient {
+        // Ktor's http client
+        return HttpClient(engine) {
+            // Install function is Ktor's client configuration function
+            install(ContentNegotiation) { // Serialization / deserialization
+                json(
+                    json = Json {
+                        ignoreUnknownKeys = true
+                    }
+                )
+            }
+
+            // Timeout configuration
+            install(HttpTimeout) {
+                socketTimeoutMillis = 5_000L
+                requestTimeoutMillis = 5_000L
+            }
+
+            // Allow Ktor's cache system
+            install(HttpCache)
+
+            defaultRequest {
+                // Here you would set up the headers
+
+                // Content type
+                contentType(ContentType.Application.Json)
+            }
+        }
+    }
+}
