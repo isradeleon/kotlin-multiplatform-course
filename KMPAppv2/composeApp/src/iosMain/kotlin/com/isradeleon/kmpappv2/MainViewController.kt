2e47aab491ecd94cbe675f97136f597c0ee767ad
Isra de Leon <isra.frontend@gmail.com>
2026-03-27 19:08:59 -0600

    set up Koin DI for iOS

--- KMPAppv2/composeApp/src/iosMain/kotlin/com/isradeleon/kmpappv2/MainViewController.kt
+++ KMPAppv2/composeApp/src/iosMain/kotlin/com/isradeleon/kmpappv2/MainViewController.kt
@@ -1,5 +1,17 @@
 package com.isradeleon.kmpappv2
 
 import androidx.compose.ui.window.ComposeUIViewController
+import com.isradeleon.kmpappv2.di.initKoin
 
-fun MainViewController() = ComposeUIViewController { App() }
+fun MainViewController() = ComposeUIViewController(
+    /**
+     * This "configure" callback is called
+     * as soon as the iOS app starts.
+     */
+    configure = {
+        /**
+         * Initializing Koin DI.
+         */
+        initKoin()
+    }
+) { App() }
