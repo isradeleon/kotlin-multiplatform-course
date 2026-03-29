a6d18e29dd171aeaac2b8283328b1a2ee7bd48f7
Isra de Leon <isra.frontend@gmail.com>
2026-03-29 11:35:44 -0600

    fix an issue with the wrong database file in iOS database builder function

--- KMPAppv2/composeApp/src/iosMain/kotlin/com/isradeleon/kmpappv2/database/getPortfolioDatabaseBuilder.kt
+++ KMPAppv2/composeApp/src/iosMain/kotlin/com/isradeleon/kmpappv2/database/getPortfolioDatabaseBuilder.kt
@@ -3,11 +3,26 @@ package com.isradeleon.kmpappv2.database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 import com.isradeleon.kmpappv2.data.local.database.PortfolioDatabase
-import platform.Foundation.NSHomeDirectory
+import kotlinx.cinterop.ExperimentalForeignApi
+import platform.Foundation.NSDocumentDirectory
+import platform.Foundation.NSFileManager
+import platform.Foundation.NSUserDomainMask
 
 fun getPortfolioDatabaseBuilder(): RoomDatabase.Builder<PortfolioDatabase> {
-    val dbFile = "${NSHomeDirectory()}/${PortfolioDatabase.DATABASE_NAME}"
+    val dbFile = "${documentDirectory()}/${PortfolioDatabase.DATABASE_NAME}"
     return Room.databaseBuilder<PortfolioDatabase>(
         name = dbFile
     )
+}
+
+@OptIn(ExperimentalForeignApi::class)
+private fun documentDirectory(): String {
+    val documentDirectory = NSFileManager.defaultManager.URLForDirectory(
+        directory = NSDocumentDirectory,
+        inDomain = NSUserDomainMask,
+        appropriateForURL = null,
+        create = false,
+        error = null,
+    )
+    return requireNotNull(documentDirectory?.path)
 }
